df70874ebec5bd347fa6da2ede69654fbc5d7e47
Joie-hsu <joie9155@gapp.nthu.edu.tw>
2025-03-25 14:19:50 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,6 +8,7 @@
 
 // 設定 Google Form 問卷網址
 const urls = [
+    'https://docs.google.com/forms/d/e/1FAIpQLSdNYHLOrJWbOCkHNM6K9v2GmDwYJwvfPQhd2lLsfgsfg205XQ/viewform?usp=dialog'
     'https://docs.google.com/forms/d/e/1FAIpQLSdFYDhfyEs9ZKeeKHO1TwE7dh_cx5aTbgevDTrVAK-WnXU9zA/viewform?usp=header',
     'https://docs.google.com/forms/d/e/1FAIpQLSfBmzSIywzUBa5IzGRcKKgiMyvcmPnb2hqEx6HePfP-FkIEqQ/viewform?usp=header',
     'https://docs.google.com/forms/d/e/1FAIpQLSf2SpgL5TX77mUfhfTeAZLcgo6vBi2G_8y29E4AW97HVdGj9w/viewform?usp=header',
@@ -23,7 +24,7 @@ const totalResponses = 2000; // 假設總共需要 2000 份問卷
 let distribution = {};
 let finalList = [];
 
-// 1. 初始化，每份問卷先分配 30 份
+// 1. 初始化，每份問卷先分配 40 份
 urls.forEach(url => {
     distribution[url] = minCount;
     for (let i = 0; i < minCount; i++) {
@@ -35,17 +36,20 @@ urls.forEach(url => {
 let remaining = totalResponses - (minCount * urls.length);
 let availableUrls = [...urls]; // 建立一個可分配額外數量的網址池
 
+// 3. 只對達到 minCount 的問卷進行隨機分配
 while (remaining > 0) {
     let randomIndex = Math.floor(Math.random() * availableUrls.length);
     let selectedUrl = availableUrls[randomIndex];
 
-    distribution[selectedUrl]++;
-    finalList.push(selectedUrl);
-
-    remaining--;
+    // 只有在某個 URL 還少於 40 次時才進行分配
+    if (distribution[selectedUrl] < minCount) {
+        distribution[selectedUrl]++;
+        finalList.push(selectedUrl);
+        remaining--;
+    }
 }
 
-// 3. 最後使用 Fisher-Yates 洗牌演算法隨機排列問卷
+// 4. 最後使用 Fisher-Yates 洗牌演算法隨機排列問卷
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -54,13 +58,11 @@ function shuffleArray(array) {
 }
 shuffleArray(finalList);
 
-// 4. 輸出結果
+// 5. 輸出結果
 console.log("隨機分配後的 2000 份問卷清單：", finalList);
 console.log("最終分配統計：", distribution);
 
-// 5. 提供隨機問卷連結函數（用於網頁應用）
+// 6. 提供隨機問卷連結函數（用於網頁應用）
 function getRandomFormLink() {
     return finalList.pop(); // 取出一個問卷，確保不會重複給到
 }
-
-// **如果在網頁應用，可用 window.location.href = getRandomFormLink();**
